07941b10b85383415150d09b9875bd502cdd5957
bhg <bhk514@hanmail.net>
2018-09-27 00:13:35 +0900

    test commit

--- sub/join_form.php
+++ sub/join_form.php
@@ -1,8 +1,8 @@
 <?php
 	if (!isset($_POST['agree1']) || !$_POST['agree1']) {
-		echo "<script>alert(\"이렇게 띄우면 되자나1\");location.replace('http://localhost/sub/join.php');</script>";	
+		echo "<script>alert(\"회원가입약관의 내용에 동의하셔야 회원가입 하실 수 있습니다.\");location.replace('http://localhost/sub/join.php');</script>";	
 	}elseif (!isset($_POST['agree2']) || !$_POST['agree2']) {
-	    echo "<script>alert(\"이렇게 띄우면 되자나2\");location.replace('http://localhost/sub/join.php');</script>";	
+	    echo "<script>alert(\"개인정보수집 이용동의 내용에 동의하셔야 회원가입 하실 수 있습니다.\");location.replace('http://localhost/sub/join.php');</script>";	
 	}
 
 ?>
