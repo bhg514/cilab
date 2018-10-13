2b51bbb40c871358b6afdac6f1f4984ce194c2b7
hsujin2 <h2sujin@gmail.com>
2018-10-13 18:18:19 +0900

    10/13 푸터 변경 다시 적용

--- footer.php
+++ footer.php
@@ -1,18 +1,29 @@
 	<footer>
 		<div class="footerInner">
-			<div class="logo_footer"><img src="../images/common/logo_footer.png" alt="CiLab"></div>
+			<div class="logo_footer"><img src="/images/common/logo_footer.png" alt="CiLab"></div>
 			<div class="text_footer">
 				<div class="footerFloor1">
 					<address>경상남도 양산시 주남로 288 그린 창업보육센터 3405호</address>
 					<p class="tel">(TEL)055-785-0710</p>
 					<p class="fax">(FAX)055-785-0711</p>
 					<p class="email">(e-mail)mmx001@cilab.kr</p>
-					<select>
+					<select onclick="perinfo()">
 						<option value="">개인정보처리방침</option>
 					</select>
 				</div>
+				<div>
+					<span class="f_name">회사명</span>
+					<span class="f_char">씨랩</span>
+					<span class="f_name">대표자</span>
+					<span class="f_char">황요섭</span>
+					<span class="f_name">사업자등록번호</span>
+					<span class="f_char">723-51-00293</span>
+					<span class="f_name">통신판매업신고번호</span>
+					<span class="f_char">2018-웅상출장소-32호</span>
+				</div>
 				<div class="footerFloor2">
-					<p class="copyright">Copyright(C) 2017. CiLab. All rights reserved.</p>
+					<p class="copyright">Copyright(C) 2017. CiLab. All rights reserved.
+					</p>
 				</div>
 			</div>
 			<div class="clear"></div>
